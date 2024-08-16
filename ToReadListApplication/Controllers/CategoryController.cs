772a05f660077e33b1e994821bc250ef8bd92321
Maathir Hamood Nasser Al-Hinaai <202016157@ocmt.edu.om>
2024-08-16 13:29:02 -0700

    Add Category Controller, Crud operations, and Views

---
+++ ToReadListApplication/Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToReadListApplication.DbContextManager;
+using ToReadListApplication.Models;
+
+namespace ToReadListApplication.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public CategoryController(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // Create 
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj == null)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                _dbcontext.Categories.Add(obj);
+                _dbcontext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+      
+        // تحديث الفئة
+        public IActionResult Update(int id)
+        {
+            var category = _dbcontext.Categories.Find(id); // البحث عن الفئة للتعديل
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Category obj) // تغيير النوع إلى Category
+        {
+            if (obj == null)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                _dbcontext.Categories.Update(obj); // تحديث الفئة
+                _dbcontext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+        }
+
+        // حذف الفئة
+        public IActionResult Delete(int id)
+        {
+            var category = _dbcontext.Categories.Find(id); // البحث عن الفئة للحذف
+            if (category is not null)
+            {
+                _dbcontext.Categories.Remove(category); // حذف الفئة
+                _dbcontext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+    }
+}
+ 
